e3ea7e9708948447d5655dc717af45501d2def90
[Juan <[juanenciso1007@gmail.com]>
2025-02-25 19:01:09 -0500

    [+] Agrega componentes dashboard & PageNotFound. Define las rutas para todos los componentes de pagina

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/public/home/home.component';
+import { LoginComponent } from './pages/public/login/login.component';
+import { RegisterComponent } from './pages/public/register/register.component';
+import { CreditsComponent } from './pages/public/credits/credits.component';
+import { DashboardComponent } from './pages/private/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/public/page-not-found/page-not-found.component';
+import { UsersComponent } from './pages/private/users/users.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'about-us', component: AboutUsComponent},
-    {path: 'contact', component: ContactComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'credits', component: CreditsComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '404', component: PageNotFoundComponent},
+    {path: 'dashboard/users', component: UsersComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: '**', redirectTo: '404', pathMatch: 'full'}
 ];
